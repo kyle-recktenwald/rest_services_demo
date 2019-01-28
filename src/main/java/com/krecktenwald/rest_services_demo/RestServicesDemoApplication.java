2bfeccc141b833e7d099e74dfec926c8cb8e3b4c
Kyle Recktenwald <Kyle.Recktenwald@arris.com>
2019-01-28 10:28:41 -0500

    Initial Setup Changes

---
+++ src/main/java/com/krecktenwald/rest_services_demo/RestServicesDemoApplication.java
@@ -0,0 +1,14 @@
+package com.krecktenwald.rest_services_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/krecktenwald/rest_services_demo/RestServicesDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.krecktenwald.rest_services_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestServicesDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
